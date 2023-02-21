955156ed7a2b0a80bc3eb51dd9ee12779b20ed8a
AmrithDSingh <amrithdathsingh04@gmail.com>
2023-02-21 16:28:47 +0000

    resturants p2

--- server.js
+++ server.js
@@ -2,13 +2,27 @@ const express = require("express");
 const app = express();
 const {Restaurant} = require("./models/index")
 const {sequelize} = require("./db");
+const { request, response } = require("express");
 
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
-
-
-app.listen(port, () => {
-    sequelize.sync();
-    console.log("Your server is listening on port " + port);
-})
+app.get("/restaurants", async (req, res) => {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  });
+  app.get("/restaurants/:id", async (req, res) => {
+    const id = req.params.id;
+    const restaurant = await Restaurant.findByPk(id);
+    res.json(restaurant);
+  });
+  app.listen(port, async () => {
+    console.log(`App listening on port ${port}`);
+  
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+    }
+  });
