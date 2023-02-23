1e828f0b8adcfc32215cda5cf81e1f98a22483bf
AmrithDSingh <amrithdathsingh04@gmail.com>
2023-02-23 16:00:20 +0000

    get resturaants p3

--- server.js
+++ server.js
@@ -26,3 +26,62 @@ app.get("/restaurants", async (req, res) => {
       console.error("Unable to connect to the database:", error);
     }
   });
+
+  app.use(express.json());
+  app.use(express.urlencoded({extended: true}));
+  //answer
+  app.post("/resturants",async (req, res) => {
+    try {
+      const restaurant = await Restaurant.create(req.body);
+      res.json(restaurant);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+  app.put("/resturants/:id",async (req, res) =>{
+    const id = req.params.id;
+    try {
+      // Find the restaurant with the given ID
+      const restaurant = await Restaurant.findByPk(id);
+  
+      if (!restaurant) {
+        // If the restaurant is not found, return a 404 (Not Found) response
+        res.status(404).json({ message: `Restaurant with ID ${id} not found` });
+      } else {
+        // Update the restaurant with the new data
+        await restaurant.update(req.body);
+  
+        // Return the updated restaurant
+        res.json(restaurant);
+      }
+    } catch (error) {
+      // If there's an error, return a 400 (Bad Request) response
+      res.status(400).json({ message: error.message });
+    }
+  });
+  app.delete("/resturants/:id",async (req, res) =>{
+    const id = req.params.id;
+    try {
+      
+      const restaurant = await Restaurant.findByPk(id);
+  
+      if (!restaurant) {
+        
+        res.status(404).json({ message: `Restaurant with ID ${id} not found` });
+      } else {
+        
+        await restaurant.destroy();
+  
+        
+        res.json({ message: `Restaurant with ID ${id} deleted successfully` });
+      }
+    } catch (error) {
+      
+      res.status(400).json({ message: error.message });
+    }
+  });
+  
+  
+  
+  
+  
